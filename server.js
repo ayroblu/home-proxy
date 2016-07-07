7e922b473ad1ad16701f8432a8c0e06e218e1110
Ben Lu <ben@ben-l.com>
2016-07-07 21:11:16 +1200

    Got it working using sni

--- server.js
+++ server.js
@@ -1,81 +1,65 @@
-var http = require('http')
+var fs = require('fs')
+, tls = require('tls')
 , https = require('https')
 , httpProxy = require('http-proxy')
-, fs = require('fs')
-, path = require('path')
+, url = require('url')
 
-var addresses = {
-  'ayro.nz': {
-    host: '127.0.0.1'
-  , port: '3000'
+var certs = {
+  "webrec.ayro.nz": {
+    key: fs.readFileSync('/etc/letsencrypt/live/webrec.ayro.nz/privkey.pem')
+  , cert: fs.readFileSync('/etc/letsencrypt/live/webrec.ayro.nz/fullchain.pem')
+  , ca: fs.readFileSync('/etc/letsencrypt/live/webrec.ayro.nz/chain.pem')
   }
-, 'webrec.ayro.nz': {
-    host: '127.0.0.1'
-  , port: '4000'
+, "ayro.nz": {
+    key: fs.readFileSync('/etc/letsencrypt/live/ayro.nz/privkey.pem')
+  , cert: fs.readFileSync('/etc/letsencrypt/live/ayro.nz/fullchain.pem')
+  , ca: fs.readFileSync('/etc/letsencrypt/live/ayro.nz/chain.pem')
   }
 }
 
-var httpsOptions = Object.keys(addresses).map(name=>{
-  if (!fs.statSync(path.join('/etc/letsencrypt/live', name, 'privkey.pem')) ||
-    !fs.statSync(path.join('/etc/letsencrypt/live', name, 'cert.pem'))) { 
-    throw new Error('Certs not found!');
+var addresses = {
+  'ayro.nz': {
+    host: 'localhost'
+  , port: '3000'
   }
-});
-var httpsOptions = Object.keys(addresses).map(name=>{
-  return {
-    key: fs.readFileSync(path.join('/etc/letsencrypt/live', name, 'privkey.pem'))
-  , cert: fs.readFileSync(path.join('/etc/letsencrypt/live', name, 'cert.pem'))
+, 'webrec.ayro.nz': {
+    host: 'localhost'
+  , port: '4000'
   }
-});
-
-var ssls = Object.keys(addresses).reduce((o, name)=>{
+}
+var ssls = Object.keys(certs).reduce((o, name)=>{
   o[name] = new httpProxy.createProxyServer({
-    ssl: httpsOptions
+    ssl: certs[name]
   , target: addresses[name]
   });
   return o;
 }, {});
+
 var proxies = Object.keys(addresses).reduce((o,name)=>{
   o[name] = new httpProxy.createProxyServer({target: addresses[name]});
   return o;
 }, {});
 
-var server = http.createServer(function(req, res){
-  if (!proxies.hasOwnProperty(req.headers.host)){
-    console.log('Could not find:', req.headers.host);
-    res.status(400).send('Bad Address');
-    return;
+var httpsOptions = {
+  SNICallback: function(hostname, cb) {
+    var ctx = tls.createSecureContext(certs[hostname])
+    cb(null, ctx)
   }
-  proxies[req.headers.host].web(req, res);
-});
-var httpsServer = https.createServer(httpsOptions['webrec.ayro.nz'], function(req, res){
-  if (!ssls.hasOwnProperty(req.headers.host)){
-    console.log('Could not find:', req.headers.host);
-    res.status(400).send('Bad Address');
-    return;
-  }
-  ssls[req.headers.host].web(req, res);
-}).listen(443);
+, key: fs.readFileSync('/etc/letsencrypt/live/ayro.nz/privkey.pem')
+, cert: fs.readFileSync('/etc/letsencrypt/live/ayro.nz/fullchain.pem')
+, ca: fs.readFileSync('/etc/letsencrypt/live/ayro.nz/chain.pem')
+}
 
-server.on('upgrade', function (req, socket, head) {
-  if (!proxies.hasOwnProperty(req.headers.host)){
-    console.log('Upgrade: could not find:', req.headers.host);
-    res.status(400).send('Bad Address');
+var server = https.createServer(httpsOptions, function(req, res){
+  console.log('hostname',url.parse('https://'+req.headers.host).hostname);
+  console.log(Object.keys(addresses), 'Index: ',Object.keys(addresses).indexOf(url.parse('https://'+req.headers.host).hostname) > -1);
+  var hostname = url.parse('https://'+req.headers.host).hostname;
+  if (Object.keys(addresses).indexOf(hostname) > -1){
+    proxies[hostname].web(req, res);
     return;
   }
-  proxies[req.headers.host].ws(req, socket, head);
 });
-httpsServer.on('upgrade', function (req, socket, head) {
-  if (!ssls.hasOwnProperty(req.headers.host)){
-    console.log('Upgrade: could not find:', req.headers.host);
-    res.status(400).send('Bad Address');
-    return;
-  }
-  ssls[req.headers.host].ws(req, socket, head);
-});
+server.listen(443);
 
-console.log('Listening on port:',process.env.PORT || 8000);
-console.log('SSL Listening on port:',443);
-server.listen(process.env.PORT || 8000);
 
 
