bdb8ee1152a32c498529dace204a3deb5200d1ca
Ben Lu <ben@ben-l.com>
2016-07-07 17:54:53 +1200

    added ssl

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http')
+, https = require('https')
 , httpProxy = require('http-proxy')
+, fs = require('fs')
+, path = require('path')
 
 var addresses = {
   'ayro.nz': {
@@ -11,6 +14,27 @@ var addresses = {
   , port: '4000'
   }
 }
+
+var httpsOptions = Object.keys(addresses).map(name=>{
+  if (!fs.statSync(path.join('/etc/letsencrypt/live', name, 'privkey.pem')) ||
+    !fs.statSync(path.join('/etc/letsencrypt/live', name, 'cert.pem'))) { 
+    throw new Error('Certs not found!');
+  }
+});
+var httpsOptions = Object.keys(addresses).map(name=>{
+  return {
+    key: fs.readFileSync(path.join('/etc/letsencrypt/live', name, 'privkey.pem'))
+  , cert: fs.readFileSync(path.join('/etc/letsencrypt/live', name, 'cert.pem'))
+  }
+});
+
+var ssls = Object.keys(addresses).reduce((o, name)=>{
+  o[name] = new httpProxy.createProxyServer({
+    ssl: httpsOptions
+  , target: addresses[name]
+  });
+  return o;
+}, {});
 var proxies = Object.keys(addresses).reduce((o,name)=>{
   o[name] = new httpProxy.createProxyServer({target: addresses[name]});
   return o;
@@ -24,6 +48,15 @@ var server = http.createServer(function(req, res){
   }
   proxies[req.headers.host].web(req, res);
 });
+var httpsServer = https.createServer(options, function(req, res){
+  if (!ssls.hasOwnProperty(req.headers.host)){
+    console.log('Could not find:', req.headers.host);
+    res.status(400).send('Bad Address');
+    return;
+  }
+  ssls[req.headers.host].web(req, res);
+}).listen(443);
+
 server.on('upgrade', function (req, socket, head) {
   if (!proxies.hasOwnProperty(req.headers.host)){
     console.log('Upgrade: could not find:', req.headers.host);
@@ -32,8 +65,17 @@ server.on('upgrade', function (req, socket, head) {
   }
   proxies[req.headers.host].ws(req, socket, head);
 });
+httpsServer.on('upgrade', function (req, socket, head) {
+  if (!ssls.hasOwnProperty(req.headers.host)){
+    console.log('Upgrade: could not find:', req.headers.host);
+    res.status(400).send('Bad Address');
+    return;
+  }
+  ssls[req.headers.host].ws(req, socket, head);
+});
 
 console.log('Listening on port:',process.env.PORT || 8000);
+console.log('SSL Listening on port:',443);
 server.listen(process.env.PORT || 8000);
 
 
